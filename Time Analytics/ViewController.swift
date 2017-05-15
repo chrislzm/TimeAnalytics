f2146f68c9006a8a35f10a2cbdae1030e708b7de
chrislzm <me@chrisleung.com>
2017-05-14 17:12:11 -0700

    Initial Commit

---
+++ Time Analytics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time Analytics
+//
+//  Created by Chris Leung on 5/14/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
