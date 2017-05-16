43efd94727866dc1644592ba816e9fd0a58b0377
chrislzm <me@chrisleung.com>
2017-05-15 21:19:30 -0700

    feat: Added table view controller to view location data

---
+++ Time Analytics/TALocationTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TALocationTableViewController.swift
+//  Time Analytics
+//
+//  Created by Chris Leung on 5/15/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import Foundation
+
+class TALocationTableViewController: TATableViewController {
+    
+}
