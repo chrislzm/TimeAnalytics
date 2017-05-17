e89be33070e37d4e7604bfed868ef11a6aed5938
chrislzm <me@chrisleung.com>
2017-05-16 22:19:59 -0700

    feat: -Added TAPlaceSegment entity that stores our interpolated place data and also has a transient property that will be used for table sections -Updated the model entity names

---
+++ Time Analytics/TAPlaceSegment+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  TAPlaceSegment+CoreDataProperties.swift
+//  Time Analytics
+//
+//  Created by Chris Leung on 5/16/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension TAPlaceSegment {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<TAPlaceSegment> {
+        return NSFetchRequest<TAPlaceSegment>(entityName: "TAPlaceSegment")
+    }
+
+    @NSManaged public var endTime: NSDate?
+    @NSManaged public var lat: Double
+    @NSManaged public var lon: Double
+    @NSManaged public var movesStartTime: NSDate?
+    @NSManaged public var startTime: NSDate?
+    @NSManaged public var name: String?
+
+}
