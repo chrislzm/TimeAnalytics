2111f1326f0de9cb7c1803698d4dfd4eeb493a78
chrislzm <me@chrisleung.com>
2017-05-21 14:55:43 -0700

    feat: Added TAPlaceDetailTableView cell for listing place visit history in a table

---
+++ Time Analytics/TAPlaceDetailTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TAPlaceDetailTableViewCell.swift
+//  Time Analytics
+//
+//  Created by Chris Leung on 5/21/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+
+import UIKit
+
+class TAPlaceDetailTableViewCell : UITableViewCell {
+    
+    // MARK: Outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeInOutLabel: UILabel!
+    @IBOutlet weak var lengthLabel: UILabel!
+}
