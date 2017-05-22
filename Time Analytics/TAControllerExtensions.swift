a90b9ab13961c1fe63031989e9dad87452fbadc9
chrislzm <me@chrisleung.com>
2017-05-22 08:42:25 -0700

    feat: Added the overlay view as a ViewController extension so we can use it anywhere

---
+++ Time Analytics/TAControllerExtensions.swift
@@ -0,0 +1,33 @@
+//
+//  TAControllerExtensions.swift
+//  Time Analytics
+//
+//  Created by Chris Leung on 5/22/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    // Creates sets up overlay attributes, hides it, and adds it to the navigation controller view hierarchy
+    func setupOverlayView(_ view:UIView, _ parent:UIView) {
+        
+        if view is TAProgressView {
+            (view as! TAProgressView).setupDefaultProperties()
+        }
+        
+        view.alpha = 0
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.tag = 100
+
+        parent.addSubview(view)
+        
+        let horizontalConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: parent, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        let widthConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: parent, attribute: NSLayoutAttribute.width, multiplier: 1, constant: 0)
+        let heightConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 50)
+        let bottomConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: parent, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
+        parent.addConstraints([horizontalConstraint,widthConstraint,heightConstraint,bottomConstraint])
+    }
+}
