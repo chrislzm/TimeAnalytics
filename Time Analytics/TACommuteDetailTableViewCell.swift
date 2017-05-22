576878eecb8806c64d2beba82ff38d095526b07a
chrislzm <me@chrisleung.com>
2017-05-22 13:35:54 -0700

    feat: Added commute detail cell

---
+++ Time Analytics/TACommuteDetailTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TADetailCommuteTableViewCell.swift
+//  Time Analytics
+//
+//  Created by Chris Leung on 5/22/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import UIKit
+
+class TACommuteDetailTableViewCell : UITableViewCell {
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var lengthLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+}
