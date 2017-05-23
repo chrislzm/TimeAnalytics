0e68c41a2257f97929cd90f4666a58216f801995
chrislzm <me@chrisleung.com>
2017-05-22 17:54:33 -0700

    feat: Added commute detail view controller

---
+++ Time Analytics/TACommuteDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TACommuteDetailViewController.swift
+//  Time Analytics
+//
+//  Created by Chris Leung on 5/22/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import UIKit
+
+class TACommuteDetailViewController: TADetailViewController {
+    // MARK: Properties
+    var startName:String?
+    var startLat:Double!
+    var startLon:Double!
+    var endName:String?
+    var endLat:Double!
+    var endLon:Double!
+}
