eba063132dee32d794099c001ce93c964dc09b5e
chrislzm <me@chrisleung.com>
2017-05-22 18:02:41 -0700

    feat: Wired outlets to class

--- Time Analytics/TACommuteDetailViewController.swift
+++ Time Analytics/TACommuteDetailViewController.swift
@@ -6,9 +6,12 @@
 //  Copyright © 2017 Chris Leung. All rights reserved.
 //
 
+import Charts
+import MapKit
 import UIKit
 
 class TACommuteDetailViewController: TADetailViewController {
+    
     // MARK: Properties
     var startName:String?
     var startLat:Double!
@@ -16,4 +19,25 @@ class TACommuteDetailViewController: TADetailViewController {
     var endName:String?
     var endLat:Double!
     var endLon:Double!
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var totalCommutesLabel: UILabel!
+    @IBOutlet weak var pastMonthTotalCommutesLabel: UILabel!
+    @IBOutlet weak var averageTimeLabel: UILabel!
+    @IBOutlet weak var totalTimeLabel: UILabel!
+    
+    @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var commuteHistoryTableHeaderLabel: UILabel!
+    @IBOutlet weak var commuteHistoryTableView: UITableView!
+    
+    @IBOutlet weak var timeBeforeDepartingTableHeaderLabel: UILabel!
+    @IBOutlet weak var timeBeforeDepartingTableView: UITableView!
+    
+    @IBOutlet weak var timeAfterArrivingTableHeaderLabel: UILabel!
+    @IBOutlet weak var timeAfterArrivingTableView: UITableView!
+    
+    
 }
