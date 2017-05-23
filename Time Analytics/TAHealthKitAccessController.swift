35255fb0f1d60ad9cbd1af54009aaf41c56c82a0
chrislzm <me@chrisleung.com>
2017-05-22 23:18:10 -0700

    feat: -Implemented methods to check for HealthKit availability and ask for permission to read Workouts, Sleep Analysis, and Mindful Sessions -Added segues based on result of the above

--- Time Analytics/TAHealthKitAccessController.swift
+++ Time Analytics/TAHealthKitAccessController.swift
@@ -11,16 +11,42 @@ import UIKit
 
 class TAHealthKitAccessController: UIViewController {
 
+    @IBAction func didPressContinue(_ sender: Any) {
+        getHealthKitPermission()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if HKHealthStore.isHealthDataAvailable() {
-            print("Health data available")
-            
             let delegate =  UIApplication.shared.delegate as! AppDelegate
             delegate.healthStore = HKHealthStore()
         } else {
-            print("Health data not available")
+            self.performSegue(withIdentifier: "NoHealthKit", sender: nil)
+        }
+    }
+    
+    func getHealthKitPermission() {
+        
+        authorizeHealthKit { (authorized,  error) -> Void in
+            if !authorized {
+                self.displayErrorAlert("There was a problem with Health Kit authorization. Please try again by going to the Settings panel.")
+                self.performSegue(withIdentifier: "NoHealthKit", sender: nil)
+            } else {
+                self.performSegue(withIdentifier: "ProcessHealthKitData", sender: nil)
+            }
+        }
+    }
+    
+    func authorizeHealthKit(completion: ((_ success: Bool, _ error: Error?) -> Void)!) {
+        
+        let healthKitStore = getHealthStore()!
+        // State the health data type(s) we want to read from HealthKit.
+        let readableTypes: Set<HKSampleType> = [HKWorkoutType.workoutType(), HKObjectType.categoryType(forIdentifier: .mindfulSession)!, HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!]
+
+        healthKitStore.requestAuthorization(toShare: nil, read: readableTypes) { (success, error) -> Void in
+            if( completion != nil ) {
+                completion(success,error)
+            }
         }
     }
 }
