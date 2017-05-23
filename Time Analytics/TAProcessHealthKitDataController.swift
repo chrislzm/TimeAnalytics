be807de3de7e47534865ed0ab962bacee63e00df
chrislzm <me@chrisleung.com>
2017-05-22 23:19:21 -0700

    feat: Added TAProcessHealthKitDataController and updated segues

---
+++ Time Analytics/TAProcessHealthKitDataController.swift
@@ -0,0 +1,14 @@
+//
+//  TAProcessHealthKitDataController.swift
+//  Time Analytics
+//
+//  Created by Chris Leung on 5/22/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import HealthKit
+import UIKit
+
+class TAProcessHealthKitDataController: UIViewController {
+    
+}
