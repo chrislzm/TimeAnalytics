532485d35b44ac2c04c4bd8ae3fa50f1aa51a2c1
chrislzm <me@chrisleung.com>
2017-05-23 23:00:42 -0700

    feat: Added class for the activity detail view

---
+++ Time Analytics/TAActivityDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TAActivityDetailViewController.swift
+//  Time Analytics
+//
+//  Created by Chris Leung on 5/23/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import Foundation
+
+class TAActivityDetailViewController: TADetailViewController {
+    
+}
