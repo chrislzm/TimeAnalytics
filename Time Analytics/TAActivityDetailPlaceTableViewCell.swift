fc1efeccf855465ab17b86d2ea19b157f6bd6ea5
chrislzm <me@chrisleung.com>
2017-05-24 09:51:27 -0700

    feat: Added class for place history cell in activity detail view

---
+++ Time Analytics/TAActivityDetailPlaceTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TAActivityDetailPlaceTableViewCell.swift
+//  Time Analytics
+//
+//  Created by Chris Leung on 5/24/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import UIKit
+
+class TAActivityDetailPlaceTableViewCell : UITableViewCell {
+
+}
