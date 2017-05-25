491f16f2295ed575d05dc9fae6ad21285cb7d7fa
chrislzm <me@chrisleung.com>
2017-05-25 14:56:03 -0700

    feat: -Wired settings view refresh button -Now subclassing TADataUpdateViewController and using methods for data refresh refactor: Moved label generation text into its own method

--- Time Analytics/TASettingsViewController.swift
+++ Time Analytics/TASettingsViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class TASettingsViewController:UIViewController {
+class TASettingsViewController:TADataUpdateViewController {
 
     @IBOutlet weak var lastUpdatedLabel: UILabel!
+    
+    @IBAction func refreshDataButtonPressed(_ sender: Any) {
+        startUpdatingWithProgressView()
+    }
     
     @IBAction func logOutButtonPressed(_ sender: Any) {
         let alert = UIAlertController(title: "Confirm Log Out", message: "All Time Analytics data will cleared. You can download your Moves data again by logging back in.", preferredStyle: UIAlertControllerStyle.alert)
@@ -24,6 +28,28 @@ class TASettingsViewController:UIViewController {
 
         navigationController?.setToolbarHidden(true, animated: true)
         
+        updateLastCheckedDate()
+    }
+    
+    override func didCompleteProcessing(_ notification:Notification) {
+        removeProgressView() { () in
+            self.updateLastCheckedDate()
+        }
+    }
+    
+    func confirmLogout(alert:UIAlertAction!) {
+        // Clear persistent data
+        TAModel.sharedInstance().deleteAllDataFor(["MovesMoveSegment","MovesPlaceSegment","TAPlaceSegment","TACommuteSegment","TAActivitySegment"])
+        
+        // Clear session variables
+        TAModel.sharedInstance().deleteMovesSessionInfo()
+        
+        // Logout, unwind and display login screen
+        self.performSegue(withIdentifier: "LogOut", sender: self)
+    }
+    
+    // MARK: View Update Methods
+    func updateLastCheckedDate() {
         let lastChecked = TANetClient.sharedInstance().movesLastChecked!
         
         let dateFormatter = DateFormatter()
@@ -34,19 +60,8 @@ class TASettingsViewController:UIViewController {
         timeFormatter.dateFormat = "h:mm a"
         
         let lastUpdatedString = "\(dateFormatter.string(from: lastChecked)) at \(timeFormatter.string(from: lastChecked))"
-
+        
         lastUpdatedLabel.text = lastUpdatedString
-    }
-    
-    func confirmLogout(alert:UIAlertAction!) {
-        // Clear persistent data
-        TAModel.sharedInstance().deleteAllDataFor(["MovesMoveSegment","MovesPlaceSegment","TAPlaceSegment","TACommuteSegment","TAActivitySegment"])
-        
-        // Clear session variables
-        TAModel.sharedInstance().deleteMovesLoginInfo()
-        
-        // Logout, unwind and display login screen
-        self.performSegue(withIdentifier: "LogOut", sender: self)
     }
 }
 
