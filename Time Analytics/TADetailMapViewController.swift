c3bb5d1bfe88315270af3f7b28310f82812abff8
chrislzm <me@chrisleung.com>
2017-05-24 17:06:38 -0700

    feat: Added new detail MapView and LineChartView controllers

---
+++ Time Analytics/TADetailMapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TADetailMapViewController.swift
+//  Time Analytics
+//
+//  Created by Chris Leung on 5/24/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import UIKit
+
+class TADetailMapViewController:UIViewController {
+    
+}
