a1773f32a13350661342a815bdcf60ddaf91d06a
chrislzm <me@chrisleung.com>
2017-05-25 00:09:07 -0700

    feat: Updated settings view controller to show last time updated with a user-friendly date string

--- Time Analytics/TASettingsViewController.swift
+++ Time Analytics/TASettingsViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class TASettingsViewController:UIViewController {
 
+    @IBOutlet weak var lastUpdatedLabel: UILabel!
+    
     @IBAction func logOutButtonPressed(_ sender: Any) {
         let alert = UIAlertController(title: "Confirm Log Out", message: "All Time Analytics data will cleared. You can download your Moves data again by logging back in.", preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "Log Out", style: UIAlertActionStyle.default, handler: confirmLogout))
         alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super .viewDidLoad()
+
+        navigationController?.setToolbarHidden(true, animated: true)
+        
+        let lastChecked = TANetClient.sharedInstance().movesLastChecked!
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        dateFormatter.doesRelativeDateFormatting = true
+        
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "h:mm a"
+        
+        let lastUpdatedString = "\(dateFormatter.string(from: lastChecked)) at \(timeFormatter.string(from: lastChecked))"
+
+        lastUpdatedLabel.text = lastUpdatedString
     }
     
     func confirmLogout(alert:UIAlertAction!) {
