b7d4e8dcfa14206246c7ea969510424c939424f6
chrislzm <me@chrisleung.com>
2017-05-25 21:38:41 -0700

    feat: App delegate now handles coordination of downloading with starting the processing of the downloaded data

--- Time Analytics/AppDelegate.swift
+++ Time Analytics/AppDelegate.swift
@@ -12,11 +12,16 @@ import HealthKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    // MARK: Properties
     var window: UIWindow?
+    var totalUpdateChunks = 0
+    var chunksUpdated = 0
     var query:String?
     let stack = CoreDataStack(modelName: "Managed Objects")!
     var healthStore = HKHealthStore()
+    
+    // MARK: Lifecycle
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
         if let query = url.query {
@@ -36,29 +41,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Restore moves session data
         TAModel.sharedInstance().loadMovesSessionData()
+        TAModel.sharedInstance().autoUpdateMovesData(TANetClient.MovesApi.Constants.AutoUpdateMinutes)
+        
+        // Listen for data updates, so we can coordinate data download completion with processing
+        NotificationCenter.default.addObserver(self, selector: #selector(AppDelegate.didProcessData(_:)), name: Notification.Name("didProcessDataChunk"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(AppDelegate.willCompleteUpdate(_:)), name: Notification.Name("willCompleteUpdate"), object: nil)
+
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    // MARK: Data [auto]update methods
+    
+    func didProcessData(_ notification:Notification) {
+        chunksUpdated += 1
+        if totalUpdateChunks == chunksUpdated {
+            TAModel.sharedInstance().generateTADataFromMovesData(nil)
+        }
     }
+    
+    func willCompleteUpdate(_ notification:Notification) {
+        // Reset properties
+        totalUpdateChunks = 0
+        chunksUpdated = 0
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        TAModel.sharedInstance().updateMovesLastChecked()
+        
+        // Save data to persistent store
+        TAModel.sharedInstance().save()
+        NotificationCenter.default.post(name: Notification.Name("didCompleteUpdate"), object: nil)
     }
 }
 
