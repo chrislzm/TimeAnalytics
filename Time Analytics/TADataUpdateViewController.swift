592e74d05b9f222869380dc6669f8ec87d71f962
chrislzm <me@chrisleung.com>
2017-05-25 21:45:37 -0700

    refactor: -Completely reworked this class since the AppDelegate is now handling handoff between download and TA Data generation -This class is now responsible only for managing the progress views that appear in the UI for displaying data update progress to the user -Moved relevant methods from other classes into this class, where they're used

--- Time Analytics/TADataUpdateViewController.swift
+++ Time Analytics/TADataUpdateViewController.swift
@@ -8,72 +8,95 @@
 
 import UIKit
 
-class TADataUpdateViewController:UIViewController {
+class TADataUpdateViewController:TAViewController {
     
-    var dataChunksToDownload:Int = 0
-    var dataChunksDownloaded:Int = 0
+    var progressView:TAProgressView!
+    
+    // MARK: Lifecycle
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: Methods for updating data
     
     func startUpdatingWithProgressView() {
         
-        let progressView = TAProgressView.instanceFromNib()
+        // Setup notifications so we know when to update the progress view
+        NotificationCenter.default.addObserver(self, selector: #selector(TADataUpdateViewController.willUpdateData(_:)), name: Notification.Name("willUpdateData"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(TADataUpdateViewController.willGenerateTAData(_:)), name: Notification.Name("willGenerateTAData"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(TADataUpdateViewController.didCompleteUpdate(_:)), name: Notification.Name("didCompleteUpdate"), object: nil)
+        
+        // Create progress view that will dismiss itself since it will know exactly how much data will be processed
+
+        TAModel.sharedInstance().downloadAndProcessNewMovesData()
+    }
+    
+    func willUpdateData(_ notification:Notification) {
+        let dataChunks = notification.object as! Int
+        progressView = TAProgressView.instanceFromNib()
+        progressView.totalProgress = Float(dataChunks)
         setupOverlayView(progressView,view)
         progressView.fadeIn(nil)
-        
-        // Setup notifications so we know when to start processing data
-        NotificationCenter.default.addObserver(self, selector: #selector(TADataUpdateViewController.didCompleteDataChunk(_:)), name: Notification.Name("didProcessDataChunk"), object: nil)
-        
-        TAModel.sharedInstance().downloadAndProcessNewMovesData() { (dataChunks, error) in
-            DispatchQueue.main.async {
-                guard error == nil else {
-                    print(error!)
-                    return
-                }
-                progressView.totalProgress = Float(dataChunks)
-                self.dataChunksToDownload = dataChunks
+    }
+    
+    func willGenerateTAData(_ notification:Notification) {
+        let dataChunks = notification.object as! Int
+        print("TADataUpdateViewController: Received willGenerateTAData - \(dataChunks) Chunks")
+        DispatchQueue.main.async {
+            if dataChunks > 0 {
+                // Create a new progress view that we will have to dismiss manually, since the number of data chunks isn't exact
+                self.progressView = TAProgressView.instanceFromNib()
+                self.setupOverlayView(self.progressView,self.view)
+                self.progressView.fadeIn(nil)
+                self.progressView.defaultText = "Processing Data"
+                self.progressView.totalProgress = Float(dataChunks)
             }
         }
     }
     
-    func didCompleteDataChunk(_ notification:Notification) {
-        dataChunksDownloaded += 1
-        if dataChunksToDownload == dataChunksDownloaded {
-            
-            // Stop observing data chunk progress
-            NotificationCenter.default.removeObserver(self)
-            
-            // Start observering for completion so we can remove the progressView when finished
-            // Setup notifications so we know when to start processing data
-            NotificationCenter.default.addObserver(self, selector: #selector(TADataUpdateViewController.didCompleteProcessing(_:)), name: Notification.Name("didCompleteProcessing"), object: nil)
-            
-            // Update our last checked time here
-            TAModel.sharedInstance().updateMovesLastChecked()
-            
-            // Start generating our data
-            TAModel.sharedInstance().generateTADataFromMovesData() { (dataChunks, error) in
-                guard error == nil else {
-                    print(error!)
-                    return
-                }
-                
-                DispatchQueue.main.async {
-                    NotificationCenter.default.removeObserver(self)
-                    if dataChunks > 0 {
-                        let progressView = TAProgressView.instanceFromNib()
-                        self.setupOverlayView(progressView,self.view)
-                        progressView.fadeIn(nil)
-                        progressView.defaultText = "Processing Data"
-                        progressView.totalProgress = Float(dataChunks)
-                    }
+    func didCompleteUpdate(_ notification:Notification) {
+        DispatchQueue.main.async {
+            print("TADataUpdateViewController: Received the didCompleteUpdate notification")
+            self.removeProgressView() { () in
+                fatalError("Thus method should be overridden by subclasses--remove the progress view and notify the user here that processing is complete")
+            }
+        }
+    }
+    
+    // Remove a progress view and its observers
+    func removeProgressView(completionHandler: (()-> Void)?) {
+        if let progressView = view.viewWithTag(100) as? TAProgressView {
+            progressView.progressView.setProgress(1.0, animated: true)
+            progressView.fadeOut() { (finished) in
+                progressView.removeFromObservers()
+                progressView.removeFromSuperview()
+                NotificationCenter.default.removeObserver(self)
+                if let closure = completionHandler {
+                    closure()
                 }
             }
         }
     }
     
-    // Subclasses can override this method to provide a completion handler to removeProgressView
-    // However, they should be sure to remove the observer and save to persistent store
-    func didCompleteProcessing(_ notification:Notification) {
-        NotificationCenter.default.removeObserver(self)
-        removeProgressView(completionHandler: nil)
-        saveAllDataToPersistentStore()
+    // Creates sets up overlay attributes, hides it, and adds it to the view hierarchy
+    func setupOverlayView(_ view:UIView, _ parent:UIView) {
+        
+        if view is TAProgressView {
+            (view as! TAProgressView).setupDefaultProperties()
+        }
+        
+        view.alpha = 0
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.tag = 100
+        
+        parent.addSubview(view)
+        
+        let horizontalConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: parent, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        let widthConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: parent, attribute: NSLayoutAttribute.width, multiplier: 1, constant: 0)
+        let heightConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 50)
+        let bottomConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: parent, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
+        parent.addConstraints([horizontalConstraint,widthConstraint,heightConstraint,bottomConstraint])
     }
 }
