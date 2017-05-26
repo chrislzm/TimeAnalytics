329b0839355d08e0abbb2cf13c3bf123fb7c2a7c
chrislzm <me@chrisleung.com>
2017-05-26 16:29:56 -0700

    feat: Added generateTimeInOutStringWithDate method for TableViewControllers to make it more clear that an activity spanned across two or more days feat: Updated generateLongDateString method to include multiple dates, if activity spanned multiple dates

--- Time Analytics/TAViewController.swift
+++ Time Analytics/TAViewController.swift
@@ -12,7 +12,6 @@ import UIKit
 class TAViewController: UIViewController {
     
     // MARK: Managed Object Description Strings
-    // TODO: Consider moving these methods to TADetailViewController
 
     func generatePlaceStringDescriptions(_ place:TAPlaceSegment,_ currentYear:String?) -> (String,String,String) {
         let startTime = place.startTime! as Date
@@ -24,7 +23,7 @@ class TAViewController: UIViewController {
     func generatePlaceStringDescriptionsFromTuple(_ startTime:Date,_ endTime:Date,_ currentYear:String?) -> (String,String,String) {
         let timeInOutString = generateTimeInOutString(startTime,endTime)
         let lengthString = generateLengthString(startTime,endTime)
-        let dateString = generateLongDateString(startTime,currentYear)
+        let dateString = generateLongDateString(startTime,endTime,currentYear)
         return (timeInOutString,lengthString,dateString)
     }
 
@@ -41,7 +40,7 @@ class TAViewController: UIViewController {
         
         let timeInOutString = generateTimeInOutString(startTime,endTime)
         let commuteLengthString = generateLengthString(startTime,endTime)
-        let dateString = generateLongDateString(startTime,currentYear)
+        let dateString = generateLongDateString(startTime,endTime,currentYear)
         
         return (timeInOutString,commuteLengthString,dateString)
     }
@@ -52,7 +51,7 @@ class TAViewController: UIViewController {
         
         let timeInOutString = generateTimeInOutString(startTime,endTime)
         let activityLengthString = generateLengthString(startTime,endTime)
-        let dateString = generateLongDateString(startTime,currentYear)
+        let dateString = generateLongDateString(startTime,endTime,currentYear)
         
         return (timeInOutString,activityLengthString,dateString)
     }
@@ -75,6 +74,31 @@ class TAViewController: UIViewController {
         return timeInOutString
     }
     
+    func generateTimeInOutStringWithDate(_ startTime:Date,_ endTime:Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h:mm a"
+        let timeIn = formatter.string(from: startTime)
+        let dayTimeIn = Calendar.current.component(.day, from: startTime)
+        let dayTimeOut = Calendar.current.component(.day, from: endTime)
+        let today = Calendar.current.component(.day, from: Date())
+        let yesterday = Calendar.current.component(.day, from: Date(timeInterval: -86400, since: Date()))
+        var timeOut:String
+        
+        if dayTimeIn == dayTimeOut {
+            timeOut = formatter.string(from: endTime)
+        } else if dayTimeOut == today {
+            timeOut = "Today"
+        } else if dayTimeOut == yesterday {
+            timeOut = "Yesterday"
+        } else {
+            formatter.dateFormat = "MMM d"
+            timeOut = formatter.string(from: endTime)
+        }
+        
+        let timeInOutString = timeIn + " - " + timeOut
+        return timeInOutString
+    }
+    
     func generateLengthString(_ startTime:Date,_ endTime:Date) -> String {
         let seconds = Int(endTime.timeIntervalSince(startTime))
         let length = StopWatch(totalSeconds: seconds)
@@ -82,10 +106,24 @@ class TAViewController: UIViewController {
         return lengthString
     }
     
-    func generateLongDateString(_ time:Date,_ currentYear:String?) -> String {
+    func generateLongDateString(_ startTime:Date,_ endTime:Date,_ currentYear:String?) -> String {
         let formatter = DateFormatter()
-        formatter.dateFormat = "E, MMM d ''yy"
-        var dateString = formatter.string(from: time)
+        var dateString:String
+        
+        let dayTimeIn = Calendar.current.component(.day, from: startTime)
+        let dayTimeOut = Calendar.current.component(.day, from: endTime)
+        
+        if dayTimeIn != dayTimeOut {
+            formatter.dateFormat = "MMM d"
+            dateString = formatter.string(from: startTime)
+            formatter.dateFormat = "MMM d ''yy"
+            let timeOut = formatter.string(from: endTime)
+            dateString.append("-\(timeOut)")
+        } else {
+            formatter.dateFormat = "E, MMM d ''yy"
+            dateString = formatter.string(from: startTime)
+        }
+        
         if let year = currentYear {
             dateString = removeYearIfSame(dateString,year,-4)
         }
@@ -113,7 +151,43 @@ class TAViewController: UIViewController {
             return dateString
         }
     }
-
+    
+    // Creates length of time string in days minutes and hours
+    struct StopWatch {
+        
+        var totalSeconds: Int
+        
+        var years: Int {
+            return totalSeconds / 31536000
+        }
+        
+        var days: Int {
+            return (totalSeconds % 31536000) / 86400
+        }
+        
+        var hours: Int {
+            return (totalSeconds % 86400) / 3600
+        }
+        
+        var minutes: Int {
+            return (totalSeconds % 3600) / 60
+        }
+        
+        var seconds: Int {
+            return totalSeconds % 60
+        }
+        
+        var simpleTimeString: String {
+            if (days > 0) {
+                return "\(days)d \(hours)h \(minutes)m"
+            } else if (hours > 0) {
+                return "\(hours)h \(minutes)m"
+            } else {
+                return "\(minutes)m"
+            }
+        }
+    }
+    
     // MARK - Data and App Methods
     
     // Returns the core data stack
@@ -125,6 +199,11 @@ class TAViewController: UIViewController {
     func getHealthStore() -> HKHealthStore {
         let delegate =  UIApplication.shared.delegate as! AppDelegate
         return delegate.healthStore
+    }
+    
+    func saveAllDataToPersistentStore() {
+        let stack = self.getCoreDataStack()
+        stack.save()
     }
     
     // MARK - View Methods
@@ -199,59 +278,5 @@ class TAViewController: UIViewController {
         detailController.type = activity.type
         
         navigationController!.pushViewController(detailController, animated: true)
-    }
-    
-    // Creates length of time string in days minutes and hours
-    struct StopWatch {
-        
-        var totalSeconds: Int
-        
-        var years: Int {
-            return totalSeconds / 31536000
-        }
-        
-        var days: Int {
-            return (totalSeconds % 31536000) / 86400
-        }
-        
-        var hours: Int {
-            return (totalSeconds % 86400) / 3600
-        }
-        
-        var minutes: Int {
-            return (totalSeconds % 3600) / 60
-        }
-        
-        var seconds: Int {
-            return totalSeconds % 60
-        }
-        
-        //simplified to what OP wanted
-        var hoursMinutesAndSeconds: (hours: Int, minutes: Int, seconds: Int) {
-            return (hours, minutes, seconds)
-        }
-        var simpleTimeString: String {
-            //let hoursText = timeText(from: hours)
-            //let minutesText = timeText(from: minutes)
-            //let secondsText = timeText(from: seconds)
-            //return "\(hoursText):\(minutesText):\(secondsText)"
-            if (days > 0) {
-                return "\(days)d \(hours)h \(minutes)m"
-            } else if (hours > 0) {
-                return "\(hours)h \(minutes)m"
-            } else {
-                return "\(minutes)m"
-            }
-            //return "\(hoursText):\(minutesText)"
-        }
-        
-        private func timeText(from number: Int) -> String {
-            return number < 10 ? "0\(number)" : "\(number)"
-        }
-    }
-
-    func saveAllDataToPersistentStore() {
-        let stack = self.getCoreDataStack()
-        stack.save()
     }
 }
