48ef581b6b172543d404e1af061bf45eb50671dc
chrislzm <me@chrisleung.com>
2017-05-26 01:08:19 -0700

    refactor: Moved these methods back into this class--we do need them in other places

--- Time Analytics/TAControllerExtensions.swift
+++ Time Analytics/TAControllerExtensions.swift
@@ -129,6 +129,42 @@ class TAViewController: UIViewController {
     
     // MARK - View Methods
     
+    // Creates sets up overlay attributes, hides it, and adds it to the view hierarchy
+    func setupOverlayView(_ view:UIView, _ parent:UIView) {
+        
+        if view is TAProgressView {
+            (view as! TAProgressView).setupDefaultProperties()
+        }
+        
+        view.alpha = 0
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.tag = 100
+        
+        parent.addSubview(view)
+        
+        let horizontalConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: parent, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        let widthConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: parent, attribute: NSLayoutAttribute.width, multiplier: 1, constant: 0)
+        let heightConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 50)
+        let bottomConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: parent, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
+        parent.addConstraints([horizontalConstraint,widthConstraint,heightConstraint,bottomConstraint])
+    }
+    
+    // Remove a progress view and its observers
+    func removeProgressView(completionHandler: (()-> Void)?) {
+        if let progressView = view.viewWithTag(100) as? TAProgressView {
+            progressView.progressView.setProgress(1.0, animated: true)
+            progressView.fadeOut() { (finished) in
+                progressView.removeFromObservers()
+                progressView.removeFromSuperview()
+                NotificationCenter.default.removeObserver(self)
+                if let closure = completionHandler {
+                    closure()
+                }
+            }
+        }
+    }
+    
     func showPlaceDetailViewController(_ place:TAPlaceSegment) {
 
         let detailController = self.storyboard!.instantiateViewController(withIdentifier: "TAPlaceDetailViewController") as! TAPlaceDetailViewController
