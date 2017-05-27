4dedbc07286e70396fc13ce99af9de8003e0d9d6
chrislzm <me@chrisleung.com>
2017-05-26 22:05:59 -0700

    refactor: Removed unused cell properties

--- Time Analytics/TAPlaceTableViewCell.swift
+++ Time Analytics/TAPlaceTableViewCell.swift
@@ -14,9 +14,4 @@ class TAPlaceTableViewCell : UITableViewCell {
     @IBOutlet weak var timeInOutLabel: UILabel!
     @IBOutlet weak var lengthLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
-    
-    // Properties
-    var lat:Double! = nil
-    var lon:Double! = nil
-    var name:String! = nil
 }
