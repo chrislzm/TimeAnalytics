6a645a653d19df789fbe6245da886d34e4eb90f4
chrislzm <me@chrisleung.com>
2017-05-26 21:52:13 -0700

    doc: Updated description and comments refactor: Moved processing start after the progress view has been setup, just to prevent any missed notifications

--- Time Analytics/TAProcessHealthKitDataController.swift
+++ Time Analytics/TAProcessHealthKitDataController.swift
@@ -2,6 +2,8 @@
 //  TAProcessHealthKitDataController.swift
 //  Time Analytics
 //
+//  Imports HealthKit data and automatically segues when complete.
+//
 //  Created by Chris Leung on 5/22/17.
 //  Copyright © 2017 Chris Leung. All rights reserved.
 //
@@ -19,22 +21,29 @@ class TAProcessHealthKitDataController: TAViewController {
         // Setup notifications so we know when we finished importing data
         NotificationCenter.default.addObserver(self, selector: #selector(TAProcessHealthKitDataController.didCompleteDataChunk(_:)), name: Notification.Name("didProcessDataChunk"), object: nil)
         
-        TAModel.sharedInstance().updateHealthKitData()
+        // There are a constant amount of steps in the data processing flow
         dataChunksToImport = TAModel.Constants.HealthKitDataChunks
+
+        // Setup the progressView
         let progressView = TAProgressView.instanceFromNib()
         progressView.defaultText = "Processing Data"
         progressView.totalProgress = Float(dataChunksToImport)
         setupOverlayView(progressView, self.view)
         progressView.fadeIn(nil)
+        
+        // Start data import
+        TAModel.sharedInstance().updateHealthKitData()
     }
     
     func didCompleteDataChunk(_ notification:Notification) {
         dataChunksImported += 1
+        // If we have completed processing
         if dataChunksImported == dataChunksToImport {
             DispatchQueue.main.async {
-                // Save to persistent data since the import was done on a background context
+                // Make sure we save data to persistent store since the import was done on a background context
                 let stack = self.getCoreDataStack()
                 stack.save()
+                
                 self.removeProgressView() { () in
                     self.performSegue(withIdentifier: "FinishedProcessingHealthKitData", sender: nil)
                 }
