1c76e83e3c839363bd2f1cd179de9fc4e384fe1f
chrislzm <me@chrisleung.com>
2017-05-26 21:45:36 -0700

    doc: Added description

--- Time Analytics/TADownloadViewController.swift
+++ Time Analytics/TADownloadViewController.swift
@@ -2,6 +2,8 @@
 //  TADownloadViewController.swift
 //  Time Analytics
 //
+//  Begins the Moves data download and processing flow. Upon completion, automatically segues to the HealthKit data import view. 
+//
 //  Created by Chris Leung on 5/22/17.
 //  Copyright © 2017 Chris Leung. All rights reserved.
 //
