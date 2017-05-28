ccd381f7f1ed213588205e036d9c3bf77f0a5ba7
chrislzm <me@chrisleung.com>
2017-05-27 17:52:40 -0700

    refactor: Moved progress view management into the shared TADataUpdateViewController and made this a subclass of it

--- Time Analytics/TAProcessHealthKitDataController.swift
+++ Time Analytics/TAProcessHealthKitDataController.swift
@@ -10,44 +10,22 @@
 
 import UIKit
 
-class TAProcessHealthKitDataController: TAViewController {
-    
-    var dataChunksToImport = 0
-    var dataChunksImported = 0
+class TAProcessHealthKitDataController: TADataUpdateViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Setup notifications so we know when we finished importing data
-        NotificationCenter.default.addObserver(self, selector: #selector(TAProcessHealthKitDataController.didCompleteDataChunk(_:)), name: Notification.Name("didProcessDataChunk"), object: nil)
-        
-        // There are a constant amount of steps in the data processing flow
-        dataChunksToImport = TAModel.Constants.HealthKitDataChunks
-
-        // Setup the progressView
-        let progressView = TAProgressView.instanceFromNib()
-        progressView.defaultText = "Processing Data"
-        progressView.totalProgress = Float(dataChunksToImport)
-        setupOverlayView(progressView, self.view)
-        progressView.fadeIn(nil)
-        
-        // Start data import
+        // Start HealthKit data import
         TAModel.sharedInstance().updateHealthKitData()
     }
     
-    func didCompleteDataChunk(_ notification:Notification) {
-        dataChunksImported += 1
-        // If we have completed processing
-        if dataChunksImported == dataChunksToImport {
-            DispatchQueue.main.async {
-                // Make sure we save data to persistent store since the import was done on a background context
-                let stack = self.getCoreDataStack()
-                stack.save()
-                
-                self.removeProgressView() { () in
-                    self.performSegue(withIdentifier: "FinishedProcessingHealthKitData", sender: nil)
-                }
-            }
+    override func didCompleteAllUpdates(_ notification: Notification) {
+        DispatchQueue.main.async {
+            super.didCompleteAllUpdates(notification)
+            // Make sure we save data to persistent store since the import was done on a background context
+            let stack = self.getCoreDataStack()
+            stack.save()
+            self.performSegue(withIdentifier: "FinishedProcessingHealthKitData", sender: nil)
         }
     }
 }
