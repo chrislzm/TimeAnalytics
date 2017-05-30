f06885c5c2e19200ecb659d8d21bfaef31adbc05
chrislzm <me@chrisleung.com>
2017-05-29 17:14:04 -0700

    refactor: Removed magic numbers, created a struct with constants

--- Time Analytics/TAViewExtensions.swift
+++ Time Analytics/TAViewExtensions.swift
@@ -10,15 +10,26 @@
 
 import UIKit
 
+struct TAViewConstants {
+    static let viewFadeInDuration:Double = 0.25
+    static let viewFadeInDelay:Double = 0.0
+    static let viewFadeInAlpha:CGFloat = 1.0
+    static let viewFadeOutDuration:Double = 0.25
+    static let viewFadeOutDelay:Double = 0.0
+    static let viewFadeOutAlpha:CGFloat = 0.0
+    
+    static let tabBarHeight:CGFloat = 40
+}
+
 extension UIView {
     
     // Fade In/Out animation for UIView objects
-    func fadeIn(duration: TimeInterval = 0.25, delay: TimeInterval = 0.0, _ completionHandler: ((_ finished:Bool) -> Void)?) {
-        UIView.animate(withDuration: duration, animations: { self.alpha = 1.0 }, completion: completionHandler)
+    func fadeIn(duration: TimeInterval = TAViewConstants.viewFadeInDuration, delay: TimeInterval = TAViewConstants.viewFadeInDelay, _ completionHandler: ((_ finished:Bool) -> Void)?) {
+        UIView.animate(withDuration: duration, animations: { self.alpha = TAViewConstants.viewFadeInAlpha }, completion: completionHandler)
     }
     
-    func fadeOut(duration: TimeInterval = 0.25, delay: TimeInterval = 0.0, _ completionHandler: ((_ finished:Bool) -> Void)?) {
-        UIView.animate(withDuration: duration, animations: { self.alpha = 0.0 }, completion: completionHandler)
+    func fadeOut(duration: TimeInterval = TAViewConstants.viewFadeInDuration, delay: TimeInterval = TAViewConstants.viewFadeOutDelay, _ completionHandler: ((_ finished:Bool) -> Void)?) {
+        UIView.animate(withDuration: duration, animations: { self.alpha = TAViewConstants.viewFadeOutAlpha }, completion: completionHandler)
     }
 }
 
@@ -27,7 +38,7 @@ extension UITabBar {
     // Adjust the default size of the tabbar to be shorter
     override open func sizeThatFits(_ size: CGSize) -> CGSize {
         var sizeThatFits = super.sizeThatFits(size)
-        sizeThatFits.height = 40
+        sizeThatFits.height = TAViewConstants.tabBarHeight
         return sizeThatFits
     }
 }
